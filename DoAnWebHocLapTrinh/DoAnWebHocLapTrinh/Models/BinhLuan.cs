b3fbca659a02c519ce1a6b2ed468a30eb2c41363
LuongXuanNhat <xuannhat1932002@gmail.com>
2023-03-06 21:02:56 +0700

    Add 3 class In Model: BaiDang, BinhLuan, User

---
+++ DoAnWebHocLapTrinh/DoAnWebHocLapTrinh/Models/BinhLuan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAnWebHocLapTrinh.Models
+{
+    public class BinhLuan
+    {
+        public int IdBinhLuan { get; set; }
+        public string NoiDungBinhLuan { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ DoAnWebHocLapTrinh/DoAnWebHocLapTrinh/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAnWebHocLapTrinh.Models
+{
+    public class User
+    {
+        public int IdUser { get; set; }
+        public string TenUser { get; set; }
+        public string Email { get; set; }
+        public string TaiKhoan { get; set; }
+        public string MatKhau { get; set; }
+    }
+}
