de0b3fd70f0693d464dc4fb48359bdc3ffdf8668
LuongXuanNhat <xuannhat1932002@gmail.com>
2023-03-06 19:45:55 +0700

    Create Project for LapTrinhWeb

---
+++ DoAnWebHocLapTrinh/DoAnWebHocLapTrinh/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DoAnWebHocLapTrinh.MvcApplication" Language="C#" %>
---
+++ DoAnWebHocLapTrinh/DoAnWebHocLapTrinh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoAnWebHocLapTrinh.Startup))]
+namespace DoAnWebHocLapTrinh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
